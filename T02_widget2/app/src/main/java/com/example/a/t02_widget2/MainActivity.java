c6f9a416552109b816c6cc6a83e281b572292ff6
Your Name <you@example.com>
2017-10-23 17:14:50 +0900

    - radio button

--- T02_widget2/app/src/main/java/com/example/a/t02_widget2/MainActivity.java
+++ T02_widget2/app/src/main/java/com/example/a/t02_widget2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.a.t02_widget2;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,
                         "hello world2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RadioGroup myRadioGroup = (RadioGroup) findViewById(R.id.myRadioGroup);
+        myRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                switch (i){
+                    case R.id.myRadio1:
+                        Toast.makeText(MainActivity.this, "radio1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.myRadio2:
+                        Toast.makeText(MainActivity.this, "radio2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.myRadio3:
+                        Toast.makeText(MainActivity.this, "radio3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
             }
         });
     }
