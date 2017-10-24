8e4f7588ff4a6e6d7f5225d8ab0b82d00b45e77a
Your Name <you@example.com>
2017-10-24 10:58:35 +0900

    put/get Extra

--- T03_Activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
+++ T03_Activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQ = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnClick(View v){
         Intent intent = new Intent(this, MyActivity.class);
-        startActivity(intent);
+        intent.putExtra("id", "abcd");
+        intent.putExtra("pw", "qwer");
+        intent.putExtra("level", 10);
+        //startActivity(intent);
+        startActivityForResult(intent,MY_REQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == MY_REQ){
+            if(resultCode == RESULT_OK){
+                String str = data.getStringExtra("myResult");
+                Toast.makeText(this, "str : "+str, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- T03_Activity/app/src/main/java/com/example/a/t03_activity/MyActivity.java
+++ T03_Activity/app/src/main/java/com/example/a/t03_activity/MyActivity.java
@@ -1,7 +1,10 @@
 package com.example.a.t03_activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+        String pw = intent.getStringExtra("pw");
+        int level = intent.getIntExtra("level",0);
+    }
+
+    public void onFinishClick(View v){
+        Intent intent = new Intent();
+        intent.putExtra("myResult", "abcdefg");
+        intent.putExtra("myValue", 1234);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "my activity detroy", Toast.LENGTH_SHORT).show();
     }
 }
