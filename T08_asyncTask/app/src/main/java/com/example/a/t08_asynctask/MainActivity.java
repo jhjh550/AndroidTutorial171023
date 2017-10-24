85047f9485c6781f3d867eab74e2fa95bb1900b7
Your Name <you@example.com>
2017-10-24 15:59:22 +0900

    async task

--- T08_asyncTask/app/src/main/java/com/example/a/t08_asynctask/MainActivity.java
+++ T08_asyncTask/app/src/main/java/com/example/a/t08_asynctask/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.a.t08_asynctask;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    class MyTask extends AsyncTask<Integer, Float, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Float... values) {
+            super.onProgressUpdate(values);
+            textView.setText("count:"+values[0]);
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            int value = integers[0];
+            for(int i=value; i<100000; i++){
+                Log.d("asynctask", "count"+i);
+                publishProgress((float)i);
+            }
+            return "doInBackground Done!";
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+
+        MyTask task = new MyTask();
+        task.execute(1000);
     }
 }
