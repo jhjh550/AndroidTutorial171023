bbd8641f1f660f304118ba1f25024b32654bdd42
Your Name <you@example.com>
2017-10-24 10:03:58 +0900

    init

--- T03_Activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
+++ T03_Activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a.t03_activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v){
+        Intent intent = new Intent(this, MyActivity.class);
+        startActivity(intent);
     }
 }
