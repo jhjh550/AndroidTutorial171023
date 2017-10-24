4af2529ec3e1b86d02fe3c92f4a52c71faae4aa5
Your Name <you@example.com>
2017-10-24 11:53:17 +0900

    - context menu

--- T04_menu/app/src/main/java/com/example/a/t04_menu/MainActivity.java
+++ T04_menu/app/src/main/java/com/example/a/t04_menu/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.a.t04_menu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        registerForContextMenu(textView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0, 100, 0, "menu1");
+        menu.add(0, 101, 0, "menu2");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId() == 100){
+            Toast.makeText(this, "context menu1", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "context menu2", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       if(item.getItemId() == R.id.myItem1){
+           Toast.makeText(this, "myItem1 select", Toast.LENGTH_SHORT).show();
+       }else{
+           Toast.makeText(this, "myItem2 select", Toast.LENGTH_SHORT).show();
+       }
+       return true;
     }
 }
