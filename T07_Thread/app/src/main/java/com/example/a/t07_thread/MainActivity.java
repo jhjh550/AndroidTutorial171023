9b3384db37a6fd2c53b9b3094d021de5fd864250
Your Name <you@example.com>
2017-10-24 14:58:45 +0900

    thread init

--- T07_Thread/app/src/main/java/com/example/a/t07_thread/MainActivity.java
+++ T07_Thread/app/src/main/java/com/example/a/t07_thread/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.a.t07_thread;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    class MyThread extends Thread{
+        @Override
+        public void run() {
+            for(int i=0; i<100000; i++){
+                Log.d("thread", "count : "+i);
+            }
+        }
+    }
+    Button myButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = (Button) findViewById(R.id.myButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyThread th = new MyThread();
+                th.start();
+            }
+        });
     }
 }
