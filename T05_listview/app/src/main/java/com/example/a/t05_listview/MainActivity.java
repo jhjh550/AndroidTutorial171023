2b47fdeb6eb7ad8d72b43e25d4872933160be8f2
Your Name <you@example.com>
2017-10-24 12:13:36 +0900

    list view inint

--- T05_listview/app/src/main/java/com/example/a/t05_listview/MainActivity.java
+++ T05_listview/app/src/main/java/com/example/a/t05_listview/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.a.t05_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] myData = {"hello", "world", "android", "oracle", "java",
+            "hello", "world", "android", "oracle", "java",
+            "hello", "world", "android", "oracle", "java"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, myData);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int postion, long id) {
+                Toast.makeText(MainActivity.this, myData[postion], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
