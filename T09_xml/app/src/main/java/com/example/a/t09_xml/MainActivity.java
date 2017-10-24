5d817e5b0a28fd1e368f626b6bdb2d04eb93f46d
Your Name <you@example.com>
2017-10-24 17:01:23 +0900

    xml pullparser

--- T09_xml/app/src/main/java/com/example/a/t09_xml/MainActivity.java
+++ T09_xml/app/src/main/java/com/example/a/t09_xml/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.a.t09_xml;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    class MyPullParser extends AsyncTask<String, Void, Void>{
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            XmlPullParserFactory factory = null;
+            try {
+                factory = XmlPullParserFactory.newInstance();
+                XmlPullParser xpp = factory.newPullParser();
+                InputStream input = new URL(strings[0]).openStream();
+                xpp.setInput(input, "utf-8");
+
+                int eventType = xpp.getEventType();
+                boolean bRead = false;
+                while(eventType != XmlPullParser.END_DOCUMENT){
+                    if(eventType == XmlPullParser.START_TAG){
+                        String tag = xpp.getName();
+                        if(tag.equals("hour") || tag.equals("day") || tag.equals("temp")
+                                || tag.equals("wfKor")){
+                            bRead = true;
+                        }
+                    }else if(eventType == XmlPullParser.TEXT){
+                        if(bRead){
+                            Log.d("weather", xpp.getText());
+                            bRead = false;
+                        }
+                    }
+                    eventType = xpp.next();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v){
+        MyPullParser task = new MyPullParser();
+        task.execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153052000");
+
     }
 }
