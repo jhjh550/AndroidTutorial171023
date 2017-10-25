26becc7abf365645289b38b8fc471155c0c3cf9f
Your Name <you@example.com>
2017-10-25 17:18:17 +0900

    service

--- T15_Service/app/src/main/java/com/example/a/t15_service/MyService.java
+++ T15_Service/app/src/main/java/com/example/a/t15_service/MyService.java
@@ -3,6 +3,7 @@ package com.example.a.t15_service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
@@ -12,5 +13,23 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("service", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("service", "onStartCommand");
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("service", "onDestroy");
     }
 }
