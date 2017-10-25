9e417c94de7b546851c2b4aab7adf9dd2eaae184
Your Name <you@example.com>
2017-10-25 13:55:07 +0900

    sharedpreferences

--- T11_pref/app/src/main/java/com/example/a/t11_pref/MainActivity.java
+++ T11_pref/app/src/main/java/com/example/a/t11_pref/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a.t11_pref;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences pref = getSharedPreferences("settings", 0);
+        String name = pref.getString("name", "");
+
+        Toast.makeText(this, "name is "+name, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences pref = getSharedPreferences("settings",0);
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putString("name", "hello");
+
+        editor.apply();
     }
 }
