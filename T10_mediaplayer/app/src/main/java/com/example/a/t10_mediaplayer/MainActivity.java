4d2a6cdcd00f43256c7241b94aaf2f9310cc0ec4
Your Name <you@example.com>
2017-10-25 11:05:43 +0900

    runtime permission

--- T10_mediaplayer/app/src/main/java/com/example/a/t10_mediaplayer/MainActivity.java
+++ T10_mediaplayer/app/src/main/java/com/example/a/t10_mediaplayer/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.a.t10_mediaplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permissions,0);
+        }
+    }
+
+    public void onPlayClick(View v){
+
+    }
+
+    public void onStopClick(View v){
+
     }
 }
