b8e1ff32b42b9dd4b8d243bfe44b23f8c62848c4
Your Name <you@example.com>
2017-10-25 16:02:02 +0900

    init

--- T14_Location/app/src/main/java/com/example/a/t14_location/MainActivity.java
+++ T14_Location/app/src/main/java/com/example/a/t14_location/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.a.t14_location;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION};
+            requestPermissions(permission,0);
+        }
+        String res = "";
+        TextView textView = (TextView) findViewById(R.id.textView);
+        LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        List<String> providers = manager.getAllProviders();
+        for(int i=0;i<providers.size(); i++){
+            res += "provider:"+providers.get(i)+" state:"+
+                    manager.isProviderEnabled( providers.get(i))+"\n";
+        }
+        textView.setText(res);
     }
 }
