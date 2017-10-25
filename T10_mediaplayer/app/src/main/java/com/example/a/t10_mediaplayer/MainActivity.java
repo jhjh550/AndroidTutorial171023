801018af42972dc16ba743aac7d47191b57e02f2
Your Name <you@example.com>
2017-10-25 11:50:08 +0900

    seekbar progress

--- T10_mediaplayer/app/src/main/java/com/example/a/t10_mediaplayer/MainActivity.java
+++ T10_mediaplayer/app/src/main/java/com/example/a/t10_mediaplayer/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.a.t10_mediaplayer;
 
 import android.Manifest;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    SeekBar seekbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekbar = (SeekBar) findViewById(R.id.seekBar);
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
@@ -20,11 +28,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    MediaPlayer mp = null;
     public void onPlayClick(View v){
+        String path = Environment.getExternalStorageDirectory()
+                +"/Download/Kalimba.mp3";
+
+        mp = new MediaPlayer();
+        try {
+            mp.setDataSource(path);
+            mp.prepare();
+            mp.start();
+            seekbar.setMax(mp.getDuration());
+            Thread th = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(mp != null){
+                        try {
+                            seekbar.setProgress(mp.getCurrentPosition());
+                        }catch (Exception e){
+                            seekbar.setProgress(0);
+                        }
+                    }
+                }
+            });
+            th.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void onStopClick(View v){
-
+        if( mp != null ){
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
     }
 }
