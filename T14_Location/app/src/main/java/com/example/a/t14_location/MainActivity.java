4b3e15bfdd8bc89fbe2b70cf44264e7870ae745c
Your Name <you@example.com>
2017-10-25 16:51:03 +0900

    location listener

--- T14_Location/app/src/main/java/com/example/a/t14_location/MainActivity.java
+++ T14_Location/app/src/main/java/com/example/a/t14_location/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.a.t14_location;
 
 import android.Manifest;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,17 +24,59 @@ public class MainActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION};
-            requestPermissions(permission,0);
+            requestPermissions(permission, 0);
         }
+        final Geocoder geocoder = new Geocoder(this);
         String res = "";
-        TextView textView = (TextView) findViewById(R.id.textView);
+        final TextView textView = (TextView) findViewById(R.id.textView);
         LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         List<String> providers = manager.getAllProviders();
-        for(int i=0;i<providers.size(); i++){
-            res += "provider:"+providers.get(i)+" state:"+
-                    manager.isProviderEnabled( providers.get(i))+"\n";
+        for (int i = 0; i < providers.size(); i++) {
+            res += "provider:" + providers.get(i) + " state:" +
+                    manager.isProviderEnabled(providers.get(i)) + "\n";
         }
         textView.setText(res);
+
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                String str = "\nlat:"+location.getLatitude()+
+                        "lon:"+location.getLongitude();
+                textView.append(str);
+
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(
+                            location.getLatitude(),
+                            location.getLongitude(), 10);
+                    Address address = addresses.get(0);
+                    Log.d("address", address.toString());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        manager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, listener);
+        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
     }
 }
