3a8f6030d76b3bbe2b12a6060f4e853f71520602
Your Name <you@example.com>
2017-10-25 15:23:21 +0900

    webview

--- T13_WebView/app/src/main/java/com/example/a/t13_webview/MainActivity.java
+++ T13_WebView/app/src/main/java/com/example/a/t13_webview/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.a.t13_webview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressDialog dlg;
+    class MyWebViewClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            dlg.show();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            dlg.dismiss();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dlg = new ProgressDialog(this);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl("http://www.daum.net");
     }
 }
