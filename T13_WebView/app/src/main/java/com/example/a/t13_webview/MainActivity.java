2309608bc2fc2c179abd3b802b6e0159940a1b20
Your Name <you@example.com>
2017-10-25 15:44:15 +0900

    webview

--- T13_WebView/app/src/main/java/com/example/a/t13_webview/MainActivity.java
+++ T13_WebView/app/src/main/java/com/example/a/t13_webview/MainActivity.java
@@ -4,23 +4,37 @@ import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
     ProgressDialog dlg;
+    ProgressBar progressBar;
     class MyWebViewClient extends WebViewClient{
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
-            dlg.show();
+//            dlg.show();
+            progressBar.setVisibility(View.VISIBLE);
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-            dlg.dismiss();
+//            dlg.dismiss();
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    class MyChromeClient extends WebChromeClient{
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            progressBar.setProgress(newProgress);
         }
     }
 
@@ -29,12 +43,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         dlg = new ProgressDialog(this);
 
         webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new MyChromeClient());
         webView.loadUrl("http://www.daum.net");
     }
 }
