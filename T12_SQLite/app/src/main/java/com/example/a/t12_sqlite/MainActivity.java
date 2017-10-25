85a9fc69a6b5b36ead3bb4a812f58df1af68eb62
Your Name <you@example.com>
2017-10-25 15:05:40 +0900

    sqlite

--- T12_SQLite/app/src/main/java/com/example/a/t12_sqlite/MainActivity.java
+++ T12_SQLite/app/src/main/java/com/example/a/t12_sqlite/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a.t12_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        TestSQLiteHandler dbHandler = new TestSQLiteHandler(this);
+        dbHandler.insert("kim", 10, "서울");
+        dbHandler.insert("홍", 11, "부산");
+        dbHandler.update("kim", 15);
+
+        String res = dbHandler.selectAll();
+        textView.setText(res);
     }
 }
