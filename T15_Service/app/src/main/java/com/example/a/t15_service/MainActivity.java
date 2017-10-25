4ab248055c6adcbd302991a9b47fb7f1d6c12956
Your Name <you@example.com>
2017-10-25 17:05:05 +0900

    service

--- T15_Service/app/src/main/java/com/example/a/t15_service/MainActivity.java
+++ T15_Service/app/src/main/java/com/example/a/t15_service/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a.t15_service;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStartClick(View v){
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
+    }
+    public void onStopClick(View v){
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
     }
 }
