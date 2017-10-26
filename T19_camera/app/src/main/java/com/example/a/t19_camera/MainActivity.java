10f1d4115165ed97ca8604b84ca3b12149345710
Your Name <you@example.com>
2017-10-26 12:17:43 +0900

    camera

--- T19_camera/app/src/main/java/com/example/a/t19_camera/MainActivity.java
+++ T19_camera/app/src/main/java/com/example/a/t19_camera/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.a.t19_camera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int TAKE_PHOTO = 100;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void onBtnClick(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == TAKE_PHOTO){
+            if(resultCode == RESULT_OK){
+                Bitmap image = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(image);
+            }
+        }
     }
 }
