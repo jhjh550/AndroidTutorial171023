6ac94452a017e840181a78b649a12fb1acb896e1
Your Name <you@example.com>
2017-10-26 11:07:36 +0900

    custom action

--- T17_BR/app/src/main/java/com/example/a/t17_br/MainActivity.java
+++ T17_BR/app/src/main/java/com/example/a/t17_br/MainActivity.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,16 @@ public class MainActivity extends AppCompatActivity {
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
-            Toast.makeText(context, "batt : "+level, Toast.LENGTH_SHORT).show();
+            String action = intent.getAction();
+
+            if(action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                Toast.makeText(context, "batt : " + level, Toast.LENGTH_SHORT).show();
+            }else if(action.equals(Intent.ACTION_BATTERY_LOW)){
+                Toast.makeText(context, "battery low", Toast.LENGTH_SHORT).show();
+            }else if (action.equals("abcdefg")){
+                Toast.makeText(context, "custom br", Toast.LENGTH_SHORT).show();
+            }
         }
     };
     @Override
@@ -29,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
-
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        filter.addAction("abcdefg");
         registerReceiver(receiver, filter);
     }
 
@@ -38,5 +48,10 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
 
         unregisterReceiver(receiver);
+    }
+
+    public void onBtnClick(View v){
+        Intent intent = new Intent("abcdefg");
+        sendBroadcast(intent);
     }
 }
