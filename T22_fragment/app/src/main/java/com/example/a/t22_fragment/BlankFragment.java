900e30bfc141ac44894e91f2ae4f8d4f453aa9b4
Your Name <you@example.com>
2017-10-27 11:26:09 +0900

    onCreateView

---
+++ T22_fragment/app/src/main/java/com/example/a/t22_fragment/BlankFragment.java
@@ -0,0 +1,42 @@
+package com.example.a.t22_fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_blank, container, false);
+        final TextView textViewCounter = v.findViewById(R.id.textViewCounter);
+        Button btnIncrease = v.findViewById(R.id.btnIncrease);
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number = Integer.parseInt(textViewCounter.getText().toString());
+                number++;
+                textViewCounter.setText(""+number);
+            }
+        });
+        return v;
+    }
+
+}
